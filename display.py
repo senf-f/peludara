83fe53e14fc476cd409b3c237b0c9f6e64eeb92e
mate.mrse <mate.mrse@intis.hr>
2023-02-18 18:35:51 +0100

    added plant dropdown

--- display.py
+++ display.py
@@ -2,23 +2,27 @@ import pandas as pd
 import plotly.express as px
 from dash import Dash, dcc, html, Input, Output
 
-year = 2022
-month = 10
-city = "Zagreb"
+biljke = ["PITOMI_KESTEN", "KOPRIVE", "BOR", "TRAVE", "LIPA", "TRPUTAC", "MASLINA", "HRAST_CRNIKA", "CRKVINA",
+          "CEMPRESI", "AMBROZIJA", "HRAST", "LOBODA", "HRAST_SP", "PELIN"]
+
+# TODO https://dash.plotly.com/interactive-graphing
+# TODO Filtrirati po: 1. biljka, 2. grad, 3. godina, 4. mjesec (try-except ako file ne postoji)
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children=f'Razina peludi: {city}, {month}. {year}.', style={
+    html.H1(children=f'Razina peludi:', style={
         'textAlign': 'center'
     }),
     html.Div(
-        dcc.Dropdown(['Zagreb', 'Split'], 'Zagreb', id='city-dropdown'), style={'width': '30%',
-                                                                                'display': 'inline-block'}),
+        dcc.Dropdown(['Zagreb', 'Split', 'Pula', 'Dubrovnik', 'Zadar'], 'Zagreb', id='city-dropdown'),
+        style={'width': '25%', 'display': 'inline-block'}),
     html.Div(
-        dcc.Dropdown([9, 10], 9, id='month-dropdown'), style={'width': '30%', 'display': 'inline-block'}),
+        dcc.Dropdown(biljke, "AMBROZIJA", id='plant-dropdown'), style={'width': '25%', 'display': 'inline-block'}),
     html.Div(
-        dcc.Dropdown([2022, 2023], 2022, id='year-dropdown'), style={'width': '30%', 'display': 'inline-block'}),
+        dcc.Dropdown(list(range(1, 13)), 9, id='month-dropdown'), style={'width': '25%', 'display': 'inline-block'}),
+    html.Div(
+        dcc.Dropdown([2022, 2023], 2022, id='year-dropdown'), style={'width': '25%', 'display': 'inline-block'}),
     dcc.Graph(id="graph"),
     html.P(
         children='Izvor podataka: Nastavni zavod za javno zdravstvo \"Dr. Andrija Štampar\", https://stampar.hr/hr/peludna-prognoza',
@@ -28,10 +32,10 @@ app.layout = html.Div([
 ])
 
 
-@app.callback(Output("graph", "figure"), Input('city-dropdown', 'value'), Input('month-dropdown', 'value'),
-              Input('year-dropdown', 'value'))
-def display_graph(i_grad, i_mjesec, i_godina):
-    file_path = f'./data/{i_godina}/{i_mjesec}/{i_grad} - AMBROZIJA pelud za {i_mjesec}.{i_godina}'
+@app.callback(Output("graph", "figure"), Input('city-dropdown', 'value'), Input('plant-dropdown', 'value'),
+              Input('month-dropdown', 'value'), Input('year-dropdown', 'value'))
+def display_graph(i_grad, i_biljka, i_mjesec, i_godina):
+    file_path = f'./data/{i_godina}/{i_mjesec}/{i_grad} - {i_biljka} pelud za {i_mjesec}.{i_godina}'
 
     df = pd.read_csv(file_path, header=None, sep='  ', engine='python')
 
